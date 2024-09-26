c0f55702837ef36c992d749329b41a8ffc383a66
Amirreza Bagheri <80455495+Dr-Bagheri@users.noreply.github.com>
2024-09-26 11:31:15 +0200

    organize the code

--- amazon-cart/scripts/amazon.js
+++ amazon-cart/scripts/amazon.js
@@ -1,3 +1,7 @@
+import {cart, addToCart} from '../data/cart.js';
+import{products} from '../data/products.js';
+
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -54,38 +58,27 @@ products.forEach((product) => {
 
 });
 
+function updateCartQuantity() {
+
+    let cartQuantity = 0;
+    
+        cart.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
-
-        let matchingItem;
-
-        cart.forEach((item) => {
-            if(productId === item.productId) {
-                matchingItem = item;
-            }
-        });
+        addToCart(productId);
+        updateCartQuantity();
         
 
-        if (matchingItem) {
-            matchingItem.quantity += 1;
-        } else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        }
-
-        let cartQuantity = 0;
-    
-        cart.forEach((item) => {
-            cartQuantity += item.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        
 
     });
 });
