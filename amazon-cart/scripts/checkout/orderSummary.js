a1a520c75237820f22985e8abd428d974151ac47
Amirreza Bagheri <80455495+Dr-Bagheri@users.noreply.github.com>
2024-09-28 19:51:33 +0200

    payment summary JS

--- amazon-cart/scripts/checkout/orderSummary.js
+++ amazon-cart/scripts/checkout/orderSummary.js
@@ -3,11 +3,9 @@ import {products,getProduct} from "../../data/products.js";
 import {formatCurrency} from "../utils/money.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import {deliveryOptions,getDeliveryOption} from "../../data/deliveryOptions.js";
+import {renderPaymentSummary} from './paymentSummary.js';
 
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-deliveryDate.format('dddd, MMMM D');
 
 export function renderOrderSummary() {
 
@@ -116,6 +114,7 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
 
         container.remove();
+        renderPaymentSummary();
     });
 
 });
@@ -125,6 +124,7 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
     });
 });
 
