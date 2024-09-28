751b8921bb95935dee0e54efe175e596ca10843c
Amirreza Bagheri <80455495+Dr-Bagheri@users.noreply.github.com>
2024-09-28 12:37:38 +0200

    fixing dates for the deliveryOptions

--- amazon-cart/data/cart.js
+++ amazon-cart/data/cart.js
@@ -1,15 +1,17 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
-if (!cart) {
-    cart = [{
+
+
+/*if (cart.length === 0) {cart = [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
+       quantity: 2
     }, {
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
         quantity: 1
     }];
 
 }
+*/
 
 
 
@@ -35,8 +37,10 @@ export function addToCart(productId) {
         } else {
             cart.push({
                 productId: productId,
-                quantity: 1
+                quantity: 1,
+                deliveryOptionId: '1'
             });
+            saveToStorage();
         }
 
         saveToStorage();
